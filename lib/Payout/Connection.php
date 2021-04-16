dd61b944799df3811d65a60f51f0a64b0813ee58
Matej Kravjar <matej.kravjar@gmail.com>
2021-04-17 00:41:16 +0200

    chore: clean up some whitespace and standards

--- lib/Payout/Connection.php
+++ lib/Payout/Connection.php
@@ -35,167 +35,165 @@ use Exception;
  * @package Payout
  * @since   0.1.0
  */
-class Connection
-{
-    const TYPE_JSON = 'application/json';
+class Connection {
 
-    /**
-     * @var string $base_url API base URL
-     * @var $curl
-     * @var array $headers HTTP request headers
-     * @var mixed $response HTTP response
-     */
-    private $base_url, $token, $curl, $headers = array(), $response;
+	const TYPE_JSON = 'application/json';
 
-    /**
-     * Connection constructor.
-     *
-     * @param string $base_url
-     */
-    public function __construct($base_url)
-    {
-        $this->base_url = $base_url;
-        $this->curl = curl_init();
+	/**
+	 * @var string $base_url API base URL
+	 * @var $curl
+	 * @var array $headers HTTP request headers
+	 * @var mixed $response HTTP response
+	 */
+	private $base_url, $token, $curl, $headers = array(), $response;
 
-        curl_setopt_array($this->curl, array(
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => false,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1
-        ));
-    }
+	/**
+	 * Connection constructor.
+	 *
+	 * @param string $base_url
+	 */
+	public function __construct( $base_url ) {
+		$this->base_url = $base_url;
+		$this->curl     = curl_init();
 
-    /**
-     * Add a custom header to the request.
-     *
-     * @param string $header HTTP request field name
-     * @param string $value HTTP request field value
-     */
-    public function addHeader($header, $value)
-    {
-        $this->headers[$header] = "$header: $value";
-    }
+		curl_setopt_array(
+			$this->curl,
+			array(
+				CURLOPT_RETURNTRANSFER => true,
+				CURLOPT_ENCODING       => '',
+				CURLOPT_MAXREDIRS      => 10,
+				CURLOPT_TIMEOUT        => 0,
+				CURLOPT_FOLLOWLOCATION => false,
+				CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
+			)
+		);
+	}
 
-    /**
-     * Clear previously cached request data and prepare for
-     * making a fresh request.
-     */
-    private function initializeRequest()
-    {
-        $this->response = '';
-        $this->addHeader('Content-Type', self::TYPE_JSON);
-        $this->addHeader('Accept', self::TYPE_JSON);
+	/**
+	 * Add a custom header to the request.
+	 *
+	 * @param string $header HTTP request field name
+	 * @param string $value HTTP request field value
+	 */
+	public function addHeader( $header, $value ) {
+		$this->headers[ $header ] = "$header: $value";
+	}
 
-        curl_setopt($this->curl, CURLOPT_HTTPHEADER, $this->headers);
-    }
+	/**
+	 * Clear previously cached request data and prepare for
+	 * making a fresh request.
+	 */
+	private function initializeRequest() {
+		$this->response = '';
+		$this->addHeader( 'Content-Type', self::TYPE_JSON );
+		$this->addHeader( 'Accept', self::TYPE_JSON );
 
-    /**
-     * Authenticate API connection. Make an HTTP POST request to the
-     * authorization endpoint  and obtain access token.
-     *
-     * @param string $url
-     * @param string $client_id Payout client ID
-     * @param string $client_secret Payout client secret
-     *
-     * @return mixed
-     * @throws Exception
-     */
-    public function authenticate($url, $client_id, $client_secret)
-    {
-        $this->initializeRequest();
+		curl_setopt( $this->curl, CURLOPT_HTTPHEADER, $this->headers );
+	}
 
-        $credentials = json_encode(array(
-            'client_id' => $client_id,
-            'client_secret' => $client_secret
-        ));
+	/**
+	 * Authenticate API connection. Make an HTTP POST request to the
+	 * authorization endpoint  and obtain access token.
+	 *
+	 * @param string $url
+	 * @param string $client_id Payout client ID
+	 * @param string $client_secret Payout client secret
+	 *
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function authenticate( $url, $client_id, $client_secret ) {
+		$this->initializeRequest();
 
-        curl_setopt($this->curl, CURLOPT_URL, $this->base_url . $url);
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $credentials);
+		$credentials = json_encode(
+			array(
+				'client_id'     => $client_id,
+				'client_secret' => $client_secret,
+			)
+		);
 
-        $this->response = curl_exec($this->curl);
+		curl_setopt( $this->curl, CURLOPT_URL, $this->base_url . $url );
+		curl_setopt( $this->curl, CURLOPT_POSTFIELDS, $credentials );
 
-        return $this->handleResponse();
-    }
+		$this->response = curl_exec( $this->curl );
 
-    /**
-     * Make an HTTP POST request to the specified endpoint.
-     *
-     * @param string $url URL to which we send the request
-     * @param mixed $body Data payload (JSON string or raw data)
-     *
-     * @return mixed
-     * @throws Exception
-     */
-    public function post($url, $body)
-    {
-        $this->addHeader('Authorization', 'Bearer ' . $this->token);
-        $this->initializeRequest();
+		return $this->handleResponse();
+	}
 
-        if (!is_string($body)) {
-            $body = json_encode($body);
-        }
+	/**
+	 * Make an HTTP POST request to the specified endpoint.
+	 *
+	 * @param string $url URL to which we send the request
+	 * @param mixed  $body Data payload (JSON string or raw data)
+	 *
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function post( $url, $body ) {
+		$this->addHeader( 'Authorization', 'Bearer ' . $this->token );
+		$this->initializeRequest();
 
-        curl_setopt($this->curl, CURLOPT_URL, $this->base_url . $url);
-        curl_setopt($this->curl, CURLOPT_POSTFIELDS, $body);
+		if ( ! is_string( $body ) ) {
+			$body = json_encode( $body );
+		}
 
-        $this->response = curl_exec($this->curl);
+		curl_setopt( $this->curl, CURLOPT_URL, $this->base_url . $url );
+		curl_setopt( $this->curl, CURLOPT_POSTFIELDS, $body );
 
-        return $this->handleResponse();
-    }
+		$this->response = curl_exec( $this->curl );
 
-    /**
-     * Make an HTTP GET request to the specified endpoint.
-     *
-     * @param string $url URL to retrieve
-     * @param array|bool $query Optional array of query string parameters
-     *
-     * @return mixed
-     * @throws Exception
-     */
-    public function get($url, $query = false)
-    {
-        $this->addHeader('Authorization', 'Bearer ' . $this->token);
-        $this->initializeRequest();
+		return $this->handleResponse();
+	}
 
-        if (is_array($query)) {
-            $url .= '?' . http_build_query($query);
-        }
+	/**
+	 * Make an HTTP GET request to the specified endpoint.
+	 *
+	 * @param string     $url URL to retrieve
+	 * @param array|bool $query Optional array of query string parameters
+	 *
+	 * @return mixed
+	 * @throws Exception
+	 */
+	public function get( $url, $query = false ) {
+		$this->addHeader( 'Authorization', 'Bearer ' . $this->token );
+		$this->initializeRequest();
 
-        curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, 'GET');
-        curl_setopt($this->curl, CURLOPT_URL, $this->base_url . $url);
-        curl_setopt($this->curl, CURLOPT_POST, false);
-        curl_setopt($this->curl, CURLOPT_PUT, false);
-        curl_setopt($this->curl, CURLOPT_HTTPGET, true);
+		if ( is_array( $query ) ) {
+			$url .= '?' . http_build_query( $query );
+		}
 
-        $this->response = curl_exec($this->curl);
+		curl_setopt( $this->curl, CURLOPT_CUSTOMREQUEST, 'GET' );
+		curl_setopt( $this->curl, CURLOPT_URL, $this->base_url . $url );
+		curl_setopt( $this->curl, CURLOPT_POST, false );
+		curl_setopt( $this->curl, CURLOPT_PUT, false );
+		curl_setopt( $this->curl, CURLOPT_HTTPGET, true );
 
-        return $this->handleResponse();
-    }
+		$this->response = curl_exec( $this->curl );
 
-    /**
-     * Check the response for possible errors and handle the response body returned.
-     *
-     * @return mixed the value encoded in json in appropriate PHP type.
-     * @throws Exception
-     */
-    private function handleResponse()
-    {
-        if (curl_error($this->curl)) {
-            throw new Exception('Payout error: ' . curl_error($this->curl));
-        }
+		return $this->handleResponse();
+	}
 
-        $response = json_decode($this->response);
+	/**
+	 * Check the response for possible errors and handle the response body returned.
+	 *
+	 * @return mixed the value encoded in json in appropriate PHP type.
+	 * @throws Exception
+	 */
+	private function handleResponse() {
+		if ( curl_error( $this->curl ) ) {
+			throw new Exception( 'Payout error: ' . curl_error( $this->curl ) );
+		}
 
-        if (isset($response->errors)) {
-            throw new Exception('Payout error: ' . $response->errors);
-        }
+		$response = json_decode( $this->response );
 
-        if (isset($response->token)) {
-            $this->token = $response->token;
-        }
+		if ( isset( $response->errors ) ) {
+			throw new Exception( 'Payout error: ' . $response->errors );
+		}
 
-        return $response;
-    }
+		if ( isset( $response->token ) ) {
+			$this->token = $response->token;
+		}
+
+		return $response;
+	}
 }
